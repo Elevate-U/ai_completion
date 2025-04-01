dd70af98a3d018397f30fa47907d9d18dcb0ef3e
evergreen <dragon_fire_36@outlook.com>
2025-04-01 02:44:35 -0500

    feat: add basic Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Add Grunt task configurations here if needed later
+  });
+
+  // Load Grunt plugins here if needed later
+  // Example: grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  // This task runs when you just type "grunt" in the terminal
+  grunt.registerTask('default', []);
+
+};
