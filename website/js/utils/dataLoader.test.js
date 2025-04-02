610248bda89c7abf48e19917a483f278168b955b
evergreen <dragon_fire_36@outlook.com>
2025-04-02 02:18:23 -0500

    changinggg

--- website/js/utils/dataLoader.test.js
+++ website/js/utils/dataLoader.test.js
@@ -1,2 +1,11 @@
 // Tests for fetchPricingData removed as the function was moved server-side.
 // See comments in dataLoader.js for details.
+
+// Placeholder test to prevent Jest errors about empty test suites.
+// TODO: Add tests for the remaining functions in dataLoader.js (loadToolData, loadAllToolsData, findToolInData)
+describe('dataLoader Utilities', () => {
+  it('should have tests added for existing functions', () => {
+    // This test doesn't do anything, it just prevents the suite from being empty.
+    expect(true).toBe(true);
+  });
+});
